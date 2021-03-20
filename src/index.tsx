bfe3bd1bd7885c1b25a12447fc7c9bcf51b8370f
즈눅 <xnuk@users.noreply.github.com>
2021-03-20 22:33:52 +0900

    그래 내가 잘못했다

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import { hydrate, render } from 'preact'
 import { Main } from './app'
 import './index.sass'
-import './index.html.tsx'
-
 
 ;(import.meta.hot ? hydrate : render)(<Main />, document.body)
