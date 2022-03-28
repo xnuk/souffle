136419b957a998c90736b6085457a66b91d20ca1
Xnuk Shuman <admin@xnu.kr>
2022-03-29 07:46:59 +0900

    bye sugarss!

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,9 +2,3 @@ interface ImportMeta {
 	url: string
 	hot: boolean
 }
-
-declare module 'sugarss' {
-	import type { Parser, Stringifier } from 'postcss'
-	export const stringify: Stringifier
-	export const parse: Parser
-}
